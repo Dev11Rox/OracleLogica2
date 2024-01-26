b647e5463121c1f1cb5f640cbf729ac03cc77f0a
Dev11Rox <roxana11r2004@gmail.com>
2024-01-26 13:54:27 -0500

    06

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ parrafo.innerHTML = 'Escribe un Numero';
 function asignarTextElem(elemento, texto){
     let elementoHTML= document.querySelector(elemento);
     elementoHTML.innerHTML=texto;
+    return;
 }
 asignarTextElem('h1','Juego de adivina el numero')
-asignarTextElem('p','Escribe un numero del 1 al 10')
+asignarTextElem('p','Escribe un numero del 1 al 20')
+
 function intentoUser(){
     alert('click desde el boton');
+    return;
 };
+
+function generarNumeroSecreto() {
+    let num = Math.floor(Math.random()*10)+1;
+    return num;
+}
+
+let numSec = generarNumeroSecreto();
+//alert(`el numero secreto es ${numSec}`);
